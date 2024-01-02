debd5acaf8876f60d4ba7363ffbd0d8c506f41a8
Alibaba <abdulmanafaliu414@gmail.com>
2024-01-02 11:41:37 +0000

    created login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="login-container">
+        <h1>Login</h1>
+        <input type="text" name="" id="" placeholder="Email" /> <br />
+        <br />
+        <input type="password" name="" id="" placeholder="Password" />
+      </div>
     </div>
   );
 }
